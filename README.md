6d9ca0b425fed86c70d324fa3ce2b46a20d84e9e
Satyam Bansal <satyam.b@turing.com>
2021-08-10 21:18:59 +0530

    Add deployed URL

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # NFT Marketplace for Artists
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/0bb74047-58c9-4725-b89c-022e2d7125af/deploy-status)](https://app.netlify.com/sites/condescending-bohr-1c93ec/deploys)
+
+Deployed URL: https://nftmarket.satyambnsal.com/
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        code: {
+          green: '#b5f4a5',
+          yellow: '#ffe484',
+          purple: '#d9a9ff',
+          red: '#ff8383',
+          blue: '#93ddfd',
+          white: '#fff',
+        },
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+  variants: {},
+  plugins: [require('@tailwindcss/ui')],
+};
